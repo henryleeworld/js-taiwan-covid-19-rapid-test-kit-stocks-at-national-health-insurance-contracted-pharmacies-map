3f81f4c73441c65790cf21906fd251a2a21bd33a
Henry Lee <henryleeworld@gmail.com>
2023-03-15 09:17:58 +0800

    feat:更新最新資料

--- js/rapid-test-kit-stocks.js
+++ js/rapid-test-kit-stocks.js
@@ -58,7 +58,7 @@ var sidebarTitle = document.getElementById('sidebarTitle');
 var content = document.getElementById('sidebarContent');
 
 var appView = new ol.View({
-    center: ol.proj.fromLonLat([120.221507, 23.000694]),
+    center: ol.proj.fromLonLat([121.518560, 25.040770]),
     zoom: 14
 });
 
